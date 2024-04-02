615f2fd13771d9f4204cbf36d4111246e891a7b8
Meghraj Mahesh Patil <114827353+mathblender17@users.noreply.github.com>
2024-04-02 08:31:07 +0530

    Create 376. Wiggle Subsequence_medium.cpp

---
+++ 376. Wiggle Subsequence_medium.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+//Here the main intuition is that the sequence should be 
+//having alterante nuber direction 
+//i.e.  greater and smaller 
+//and we need to count till what lenght 
+// this sequence is manintained - done usinf peaks and valleys
+    int wiggleMaxLength(vector<int>& nums) {
+        int size=nums.size(), peak=1, valley=1;
+        for(int i=1; i<size; ++i){
+                 if(nums[i]>nums[i-1]) peak = valley + 1;
+            else if(nums[i]<nums[i-1]) valley = peak + 1;
+        }
+        return max(peak , valley );
+    }
+};
