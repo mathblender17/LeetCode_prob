3f85fca5ae31b01252a53fbf197597dc41f3dedc
Meghraj Mahesh Patil <114827353+mathblender17@users.noreply.github.com>
2024-09-11 04:24:28 +0530

    Update 287. Find the Duplicate Number_medium.cpp

--- 287. Find the Duplicate Number_medium.cpp
+++ 287. Find the Duplicate Number_medium.cpp
@@ -3,7 +3,7 @@ public:// TC O(N)  technically 2n as the array is travesed twice
 //        SC O(1)
 
 // Floyd's tortoise and hare algo  - i.e fast and slow pointers
-// we treat this array/vector as a linkedlist and find ac cycle 
+// we treat this array/vector as a linkedlist and find ac cycle {here the nums[i] = j, pointer from index i to index j.}
 // 1 trwsversal is for finding the cycl;e existance
 // 2nd one is for finding the start of the cycle - i.e finding the number repeated
     int findDuplicate(vector<int>& nums) {
