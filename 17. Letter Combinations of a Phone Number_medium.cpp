e21a92bdad67924f2c37134dc6f8724adddda836
Meghraj Mahesh Patil <114827353+mathblender17@users.noreply.github.com>
2025-01-23 17:11:19 +0530

    Update 17. Letter Combinations of a Phone Number_medium.cpp
    updates the comments

--- 17. Letter Combinations of a Phone Number_medium.cpp
+++ 17. Letter Combinations of a Phone Number_medium.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
 /**
-Keeping in mind the basic recursion flow - aslo base case 
+CORE IDEA : recursion + backtracking + with proper loops for the chars
 **/
-    void help(int i,string &s,string &temp,vector<string> &ans, unordered_map<char,string> &m){
-        if(i==s.size()){
-            if(!temp.empty()){
-                ans.push_back(temp);
-            }
-            return;
-        }
-
-
-        string str=m[s[i]];
-        for(int j=0;j<str.size();j++){
-            temp.push_back(str[j]);
-            help(i+1,s,temp,ans,m);
-            temp.pop_back();
-        }
-
-    }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,string> m;
+        unordered_map<char,string> m;//to store the values related to each char
+        vector<string> res;
         m['2']="abc";
         m['3']="def";
         m['4']="ghi";
@@ -30,11 +14,25 @@ Keeping in mind the basic recursion flow - aslo base case
         m['7']="pqrs";
         m['8']="tuv";
         m['9']="wxyz";
-        vector<string> ans;
         string temp;
-        help(0,digits,temp,ans,m);
-        return ans;
+        backtrack_pnc(0,digits,temp,res,m);
+        return res;
+    }
+    void backtrack_pnc(int i,string& digits,string& temp,vector<string>& res,unordered_map<char,string>& m){
+        //i keeps the track of levels of the tree or the length of each string in the results
+        //
+        if(i==digits.size()){//permutation hist it's limit - result permutation has to be of the same length of the no. of digits
+            if(!temp.empty()){
+                res.push_back(temp);
+            }
+                return;
+        }
 
-
+        string str = m[digits[i]];// at each level - decides the set of chars - if "23" 1st level str =abc and 2nd level str=def
+        for(int j=0;j<str.size();j++){//to iterate through all the char in str - same level of tree
+            temp.push_back(str[j]);
+            backtrack_pnc(i+1,digits,temp,res,m);
+            temp.pop_back();
+        }
     }
 };
